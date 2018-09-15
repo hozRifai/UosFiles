35d71dbd2cea2b6e17eb909e7d86f48f3a6025f5
hozrifai <p-p-h@hotmail.com>
2018-09-15 21:46:23 +0400

    uploading last version

--- files/api/views.py
+++ files/api/views.py
@@ -2,12 +2,14 @@ from rest_framework import generics , mixins
 from files.models import Document
 from .serializers import DocumentPostSerializer
 
+
 class FileRUDDocument(generics.RetrieveUpdateDestroyAPIView):
     lookup_field = "document"
     serializer_class = DocumentPostSerializer
-    
+
     def get_queryset(self):
         return Document.objects.all()
+
 
 class UploadDocument(mixins.CreateModelMixin, generics.ListAPIView):
     lookup_field = "document"
@@ -18,4 +20,3 @@ class UploadDocument(mixins.CreateModelMixin, generics.ListAPIView):
 
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
-
